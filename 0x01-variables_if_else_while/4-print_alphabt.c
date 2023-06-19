19f43a12d4e005186b639c3c87a68d6c6292c203
Othniel01 <othnielabalaka@gmail.com>
2023-06-19 02:50:53 -0700

    Hmmm

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,16 +3,23 @@
 /**
  * main - Entry point
  *
+ * Description: Program to print the alphabet in lowercase
+ *              excluding the letters 'q' and 'e'
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 char letter;
+
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 if (letter != 'q' && letter != 'e')
+{
 putchar(letter);
 }
+}
+
 putchar('\n');
 return (0);
-}`
+}
