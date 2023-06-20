093e22bb413c3b82d102c6cc40c82115a8a69c86
Othniel01 <othnielabalaka@gmail.com>
2023-06-20 16:31:32 -0700

    <<<<<<>>>>>>>>

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,45 @@
 #include "main.h"
 
+#define MAX_DIGITS 100
+
 /**
  * print_large_number - Prints a large number
  * @num: The number to print
  */
-void print_large_number(unsigned long num)
+void print_large_number(unsigned int *num)
 {
-	unsigned long temp = num;
-	unsigned long rev_num = 0;
+	int i;
+	int start_printing = 0;
 
-	while (temp != 0)
+	for (i = MAX_DIGITS - 1; i >= 0; i--)
 	{
-		rev_num = rev_num * 10 + temp % 10;
-		temp /= 10;
+		if (start_printing || num[i] != 0)
+		{
+			_putchar(num[i] + '0');
+			start_printing = 1;
+		}
 	}
 
-	while (rev_num != 0)
+	if (!start_printing)
+		_putchar('0');
+}
+
+/**
+ * add_numbers - Adds two large numbers
+ * @num1: The first number to add
+ * @num2: The second number to add
+ * @result: The result of the addition
+ */
+void add_numbers(unsigned int *num1, unsigned int *num2, unsigned int *result)
+{
+	int carry = 0;
+	int i;
+
+	for (i = 0; i < MAX_DIGITS; i++)
 	{
-		_putchar((rev_num % 10) + '0');
-		rev_num /= 10;
+		int sum = num1[i] + num2[i] + carry;
+		result[i] = sum % 10;
+		carry = sum / 10;
 	}
 }
 
@@ -27,8 +48,13 @@ void print_large_number(unsigned long num)
  */
 void print_fibonacci(void)
 {
-	unsigned long fib1 = 1, fib2 = 2, fib3;
+	unsigned int fib1[MAX_DIGITS] = {0};
+	unsigned int fib2[MAX_DIGITS] = {0};
+	unsigned int fib3[MAX_DIGITS] = {0};
 	int i;
+
+	fib1[MAX_DIGITS - 1] = 1;
+	fib2[MAX_DIGITS - 1] = 2;
 
 	print_large_number(fib1);
 	_putchar(',');
@@ -37,14 +63,14 @@ void print_fibonacci(void)
 
 	for (i = 2; i < 98; i++)
 	{
-		fib3 = fib1 + fib2;
+		add_numbers(fib1, fib2, fib3);
 
 		_putchar(',');
 		_putchar(' ');
 		print_large_number(fib3);
 
-		fib1 = fib2;
-		fib2 = fib3;
+		memcpy(fib1, fib2, sizeof(fib1));
+		memcpy(fib2, fib3, sizeof(fib2));
 	}
 
 	_putchar('\n');
