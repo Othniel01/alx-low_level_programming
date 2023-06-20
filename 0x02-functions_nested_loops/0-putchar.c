5a2f29128a820813acd924ec4d2a50332ce2e86a
Othniel01 <othnielabalaka@gmail.com>
2023-06-20 15:00:02 -0700

    hmmm

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-printf("_putchar \n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
 return (0);
 }
