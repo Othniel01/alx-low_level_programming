dcd371f801c7426ea644c53b2dbd53c26f8fc9c0
Othniel01 <othnielabalaka@gmail.com>
2023-06-23 03:30:57 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - prints the numbers, followed by a new line
+*
+* Return: void
+*/
+void print_numbers(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		_putchar(number + '0');
+		number++;
+	}
+	_putchar('\n');
+}
