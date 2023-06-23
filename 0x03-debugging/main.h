8abc9912cf339da31a9c3f93b73e73fc26d96709
Othniel01 <othnielabalaka@gmail.com>
2023-06-23 02:46:57 -0700

    fix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 #include <stdio.h>
 
-int largest_number(int a, int b, int c); // Function prototype for finding the largest number
-void print_remaining_days(int month, int day, int year); // Function prototype for printing the remaining days in a year
-int convert_day(int month, int day); // Function prototype for converting a date to the day of the year
-void positive_or_negative(int i); // Function prototype for determining if a number is positive or negative
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void positive_or_negative(int i);
 #endif
