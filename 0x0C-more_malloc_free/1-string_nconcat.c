3e71c908d55c33a86e12302cc4e58f3d1008720e
Othniel01 <othnielabalaka@gmail.com>
2023-07-13 09:27:44 -0700

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,41 +5,41 @@ unsigned int _strlen(char *str);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int len1, len2, i, j;
-    char *concat;
+	unsigned int len1, len2, i, j;
+	char *concat;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    len1 = _strlen(s1);
-    len2 = _strlen(s2);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
-    if (n >= len2)
-        n = len2;
+	if (n >= len2)
+		n = len2;
 
-    concat = malloc((len1 + n + 1) * sizeof(char));
-    if (concat == NULL)
-        return NULL;
+	concat = malloc((len1 + n + 1) * sizeof(char));
+	if (concat == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
 
-    for (j = 0; j < n; j++, i++)
-        concat[i] = s2[j];
+	for (j = 0; j < n; j++, i++)
+		concat[i] = s2[j];
 
-    concat[i] = '\0';
+	concat[i] = '\0';
 
-    return concat;
+	return (concat);
 }
 
 unsigned int _strlen(char *str)
 {
-    unsigned int len = 0;
+	unsigned int len = 0;
 
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    return len;
+	return (len);
 }
