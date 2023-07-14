48454eddfe13d8eaee3158a11ff2da1ad73752de
Othniel01 <othnielabalaka@gmail.com>
2023-07-14 02:27:42 -0700

    fix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,35 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing
- */
-void simple_print_buffer(int *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
 
 /**
  * array_range - creates an array of integers
@@ -40,36 +9,19 @@ void simple_print_buffer(int *buffer, unsigned int size)
  */
 int *array_range(int min, int max)
 {
-    int *arr;
-    int i, size;
+	int *arr;
+	int size, i;
 
-    if (min > max)
-        return (NULL);
+	if (min > max)
+		return (NULL);
 
-    size = max - min + 1;
-    arr = malloc(sizeof(int) * size);
+	size = max - min + 1;
+	arr = malloc(sizeof(int) * size);
+	if (arr == NULL)
+		return (NULL);
 
-    if (arr == NULL)
-        return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = min++;
 
-    for (i = 0; i < size; i++)
-        arr[i] = min++;
-
-    return (arr);
+	return (arr);
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0
- */
-int main(void)
-{
-    int *a;
-
-    a = array_range(0, 10);
-    simple_print_buffer(a, 11);
-    free(a);
-    return (0);
-}
-
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,14 +3,9 @@
 
 #include <stdlib.h>
 
-int _putchar(char c);
 void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
-unsigned int _strlen(char *str);
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-void *_memcpy(void *dest, const void *src, unsigned int n);
-unsigned int _strlen(char *s);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 
 #endif /* MAIN_H */
