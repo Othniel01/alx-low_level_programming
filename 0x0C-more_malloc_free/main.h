2bf37cafa9b5e78967bdfe2d2dd85f33e951b547
Othniel01 <othnielabalaka@gmail.com>
2023-07-14 02:23:59 -0700

    fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,5 +8,9 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 unsigned int _strlen(char *str);
+char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+void *_memcpy(void *dest, const void *src, unsigned int n);
+unsigned int _strlen(char *s);
+int *array_range(int min, int max);
 
 #endif /* MAIN_H */
